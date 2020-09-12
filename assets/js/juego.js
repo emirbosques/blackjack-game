bc205b8eecb3e92bbef2be8004433deb37d08b46
Emir Bosques <emirbosques@gmail.com>
2020-09-12 21:16:03 +0200

    WIP- Config Inicial

---
+++ assets/js/juego.js
@@ -0,0 +1,34 @@
+
+/**
+ * 2C - TWO OF CUBS
+ * 2D - TWO OF DIAMONDS
+ * 2H - TWO OF HEART
+ * 2S - TWO OF SPADES
+ */
+
+ let deck = [];
+ const tipos = ['C', 'D', 'H', 'S'];
+ const specials = ['A' ,'J', 'Q', 'K'];
+
+
+ const crearDeck = ()=>{
+     for (let i = 2; i <= 10; i++) {
+         for (const tipo of tipos) {
+             deck.push(i+tipo)
+         }
+     }
+
+     for (const tipo of tipos) {
+         for (const esp of specials) {
+             deck.push(tipo+esp)
+         }
+     }
+
+     console.log('Deck normal', deck);
+     
+     deck = _.shuffle( deck );
+     
+     console.log('Deck Shuffle', deck);
+ }
+
+ crearDeck();
